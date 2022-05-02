b6bb4e03377e3d0b9a7f81a6f6c3282bd879394f
malornh <malornh@dir.bg>
2022-05-02 15:57:08 +0300

    working version

---
+++ eTickets/Data/Services/ActorsService.cs
@@ -0,0 +1,45 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public class ActorsService : IActorsService
+    {
+        private readonly AppDbContext _context;
+        public ActorsService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Actor> GetAll()
+        {
+            var result = _context.Actors.ToList();
+            return result;
+        }
+
+        public Actor GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Actor Update(int id, Actor newActor)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
